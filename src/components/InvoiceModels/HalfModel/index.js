a041ba0e5d0f48aacd4b4bcc596c58112b604d95
Bruno Vieira Carlini Charamba <brunocharamba@gmail.com>
2020-06-12 20:57:35 -0300

    temp

--- src/components/InvoiceModels/HalfModel/index.js
+++ src/components/InvoiceModels/HalfModel/index.js
@@ -2,24 +2,64 @@ import React from 'react'
 import { Select } from 'antd'
 
 import 'antd/dist/antd.css'
-import { Container } from './styles'
+import { Container, Information, Fill, FillD, Data, HorizontalSeparator } from './styles'
+
+const emmiterBase = {
+  name: '',
+  phone: '',
+  cellphone: '',
+  document: {
+    type: 'CPF',
+    number: '',
+    extraNumber: '',
+  },
+  email: '',
+  site: '',
+  address: {
+    cep: '',
+    address: '',
+    number: '',
+    comp: '',
+    city: '',
+    state: '',
+    district: '',
+  },
+  logo: '',
+}
 
 function HalfModel() {
   const { Option } = Select
 
+  // <Container>
+  //   <Select
+  //     showSearch
+  //     style={{ width: 200 }}
+  //     placeholder="Select a person"
+  //     optionFilterProp="children"
+  //     filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+  //   >
+  //     <Option value="jack">Jack</Option>
+  //     <Option value="lucy">Lucy</Option>
+  //     <Option value="tom">Tom</Option>
+  //   </Select>
+  // </Container>
+
   return (
     <Container>
-      <Select
-        showSearch
-        style={{ width: 200 }}
-        placeholder="Select a person"
-        optionFilterProp="children"
-        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-      >
-        <Option value="jack">Jack</Option>
-        <Option value="lucy">Lucy</Option>
-        <Option value="tom">Tom</Option>
-      </Select>
+      <Information>
+        <div id="logo">LOGO</div>
+        <div id="title">Joana Julião</div>
+        <div id="company">
+          <h5>http://www.joanajuliao.com.br</h5>
+          <h5>info.joanajuliao@gmail.com</h5>
+          <h5>21.227.107/0001-47</h5>
+        </div>
+        <HorizontalSeparator />
+        <div id="photo">FOTO</div>
+      </Information>
+      <Fill />
+      <FillD />
+      <Data>s</Data>
     </Container>
   )
 }
--- src/components/InvoiceModels/HalfModel/styles.js
+++ src/components/InvoiceModels/HalfModel/styles.js
@@ -1,18 +1,21 @@
 import styled from 'styled-components'
+import { colors } from '../../../styles'
 
 const Container = styled.div`
   /* @import url('https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap'); */
+  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap');
   display: flex;
   flex: 1;
-  flex-direction: column;
-  min-height: 1310px;
-  width: 936px;
-  max-width: 936px;
+  flex-direction: row;
+  min-height: 1400px;
+  width: 1000px;
+  max-width: 1000px;
   margin: 10px;
-  background-color: green;
-  padding: 20px;
+  border: 1px solid black;
+
   & * {
     /* font-family: 'Julius Sans One', sans-serif !important; */
+    font-family: 'Rubik', sans-serif;
   }
 
   h1,
@@ -24,4 +27,71 @@ const Container = styled.div`
   }
 `
 
-export { Container }
+const Information = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 280px;
+  padding: 20px;
+  background-color: #20638f;
+
+  #logo,
+  #photo {
+    display: flex;
+    border-radius: 50px;
+    width: 100px;
+    height: 100px;
+    background-color: ${colors.midnightBlue};
+    color: ${colors.clouds};
+    justify-content: center;
+    align-items: center;
+  }
+
+  #title {
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 26px;
+    margin: 20px 0 0 0;
+    color: ${colors.clouds};
+  }
+
+  #company {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    width: 100%;
+    align-items: flex-end;
+    justify-content: flex-start;
+    margin: 20px 0 0 0;
+
+    h5 {
+      color: ${colors.clouds};
+    }
+  }
+`
+
+const Fill = styled.div`
+  width: 20px;
+  background-color: #2980b9;
+`
+
+const FillD = styled.div`
+  width: 20px;
+  background-color: #3498db;
+`
+
+const Data = styled.div`
+  width: 680px;
+  padding: 20px;
+  background-color: white;
+`
+
+const HorizontalSeparator = styled.div`
+  height: 2px;
+  width: 50%;
+  margin: 20px 0;
+  background-color: ${colors.clouds};
+`
+
+export { Container, Information, Data, Fill, FillD, HorizontalSeparator }
